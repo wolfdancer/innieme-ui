7e70f91a13dabedeae08ac58b3addc7563cd0b82
Shane <wolfdancer@users.noreply.github.com>
2025-05-21 13:01:56 -0700

    add reactmarkdown mock to see if it fixes the CI/CD problem

---
+++ client/src/__mocks__/react-markdown.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// A simple mock that renders children as-is with skipHtml prop support
+const ReactMarkdown = ({ 
+  children, 
+  skipHtml 
+}: { 
+  children: React.ReactNode, 
+  skipHtml?: boolean 
+}) => {
+  return <div data-testid="markdown-content">{children}</div>;
+};
+
+export default ReactMarkdown;
